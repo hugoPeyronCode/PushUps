8131cb42e408a71d21828eb351b8bfb40a24c803
Hugo Peyron <hugopeyron@gmail.com>
2024-03-03 19:22:21 +0100

    Initial Commit

---
+++ PushUps/PushUpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushUpsApp.swift
+//  PushUps
+//
+//  Created by Hugo Peyron on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PushUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
