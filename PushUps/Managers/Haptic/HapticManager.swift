a7fdf170df20d5490ba055e4a958ce44c6f25777
hugoPeyronCode <110022126+hugoPeyronCode@users.noreply.github.com>
2024-04-21 19:49:54 +0200

    Added haptic manager

---
+++ PushUps/Managers/Haptic/HapticManager.swift
@@ -0,0 +1,8 @@
+//
+//  HapticManager.swift
+//  PushUps
+//
+//  Created by Hugo Peyron on 21/04/2024.
+//
+
+import Foundation
---
+++ PushUps/Managers/Haptic/HapticTestView.swift
@@ -0,0 +1,18 @@
+//
+//  HapticTestView.swift
+//  PushUps
+//
+//  Created by Hugo Peyron on 21/04/2024.
+//
+
+import SwiftUI
+
+struct HapticTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticTestView()
+}
